4bc5462b28f06330a63db373b375dd0b4e8b0ff9
Chris Zuber <shgysk8zer0@gmail.com>
2024-04-16 09:46:27 -0700

    Fix `externals` preventing builds

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 export const external = [
 	'@shgysk8zer0/npm-utils/fs', '@shgysk8zer0/npm-utils/json', '@shgysk8zer0/npm-utils/yaml',
 	'@shgysk8zer0/npm-utils/utils', '@shgysk8zer0/npm-utils/consts',
-	'@shgysk8zer0/npm-utils/mimes', '@shgysk8zer0/npm-utils/exts',
+	'@shgysk8zer0/consts/mimes', '@shgysk8zer0/consts/exts',
 	'cheerio', 'commander', 'node:path',
 ];
 
